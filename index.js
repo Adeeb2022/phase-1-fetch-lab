ab2e63cdad14335db8b09ecf86b35d0d8813f3b5
Adeeb2022 <adeebsamad01@gmail.com>
2023-02-12 23:58:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(books => renderBooks(books));
   
 }
 
@@ -7,7 +9,7 @@ function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHTML = book['name'];
     main.appendChild(h2);
   });
 }
